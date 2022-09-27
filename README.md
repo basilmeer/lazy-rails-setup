7e2030708cec621b8a6215ca3eaaedc17759edb7
Basil Meer <basilmeer@gmail.com>
2022-09-27 23:04:40 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Lazy Rails Setup
+
+A small script for saving time when setting up a Linux environment for Rails
+
+## How to use
+- Clone repo
+- `cd lazy_rails_setup`
+- `chmod +x ./setup.sh`
+- `./setup.sh`
+
+This will install Ruby 3 and Rails 7 by default as of this README. In case `rvm use <version-number>` does not work, it should notify you what's up but in case you're still confused, read the note below.
+
+## Note
+Be sure to enable 'Run command as a login shell' for your terminal emulator
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Updating system packages..."
+sudo apt update
+
+echo "Upgrading system packages..."
+sudo apt upgrade -y
+
+echo "Installing gnupg2..."
+sudo apt install gnupg2
+
+echo "Installing curl (in case it doesn't exist)..."
+sudo apt install curl
+
+echo "Adding rvm gpg2 key..."
+gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+echo "Setting up rvm.sh..."
+\curl -sSL https://get.rvm.io -o rvm.sh
+cat rvm.sh | bash -s stable --rails
+source ~/.rvm/scripts/rvm
+
+echo "Setting up yarn..."
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+echo "Setting up nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+echo "Running sudo apt update..."
+sudo apt update
+
+echo "Installing Postgres, RVM and Yarn..."
+sudo apt install -y postgresql postgresql-contrib libpq-dev software-properties-common rvm yarn
+
+echo "Reloading bash config..."
+echo 'source ~/.bashrc'
